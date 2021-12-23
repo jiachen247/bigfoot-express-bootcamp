11dff633918f93751aefefb98f3dae14b9b433b8
jiachen <jiachen247@users.noreply.github.com>
2021-12-23 09:49:18 +0800

    Add express boilerplate code to index.js
    https://bootcamp.rocketacademy.co/3-backend-applications/3.ice-in-class-exercises/3.ice.1-bigfoot
    **Update the request handler callback** in the gitbook seems to imply the express bolierplate code should be there!

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+
+const handleIncomingRequest = (request, response) => {
+  console.log('request came in');
+  response.send('yay');
+};
+
+app.get('/', handleIncomingRequest);
+
+app.listen(3004);
